a6bedb1bd4274d93d75c8c4b37bd8c964656a008
Enrique Sanchez Castañeda <xyxyxy27.79@gmail.com>
2021-05-19 23:51:55 -0500

    Archivos java, esto es una prueba

---
+++ ejemplo.java
@@ -0,0 +1,18 @@
+
+public class ejemplo{
+	public static void main(String[] args){
+		int variable = 10;
+
+
+		switch(variable){
+			case 10:
+				variable = variable + 19;
+				break;
+			default:
+			System.out.println("No ingresaste el numero correcto");
+
+		}
+
+		System.out.print(variable);
+	}		
+}
